4316472c9e5c610d3f8b084de3b3f93dedc72dca
potalchuck <potalchuck@mail.ru>
2024-06-26 17:18:58 +0300

    Add files via upload

---
+++ homework2.py.py
@@ -0,0 +1,5 @@
+Number_of_completed_Hw=12
+Number_of_hours_spent=1.5
+Course_name="Python"
+Time_per_task=0.125
+print("Курс:",Course_name,",всего задач:",Number_of_completed_Hw,",затрачено часов:",Number_of_hours_spent,", среднее время выполнения",Time_per_task,"часа.")
