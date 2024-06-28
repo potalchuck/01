69713ce8c53550afa461db10552a3ed8ffd5e5a7
potalchuck <potalchuck@mail.ru>
2024-06-28 16:40:31 +0300

    Add files via upload

---
+++ main.py.py
@@ -0,0 +1,12 @@
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+print(1234%1000//10+5678%1000//10)
+#4th program
+one1 = 13.42
+two2 = 42.13
+part1 = int(str(one1).split(".")[1])
+part2 = int(str(two2).split(".")[1])
+print(int(one1) == part2 or int(two2) == part1)
