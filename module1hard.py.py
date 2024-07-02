7aa0dd008f7353b49d1b7611b5f645df9df886f6
potalchuck <potalchuck@mail.ru>
2024-07-02 17:59:57 +0300

    Add files via upload

---
+++ module1hard.py.py
@@ -0,0 +1,7 @@
+A=[5,3,3,5,4]
+B=[2,2,2,3]
+J=[4,5,5,2]
+K=[4,4,3]
+S=[5,5,5,4,5,]
+students={"Aaron":sum(A)/len(A),"Bilbo":sum(B)/len(B),"Jonny":sum(J)/len(J),"Kendrik":sum(K)/len(K),"Stive":sum(S)/len(S)}
+print(students)
