6972d8c8ab90422e5f83abf4f2b43eaacc20c78b
potalchuck <potalchuck@mail.ru>
2024-07-08 16:35:56 +0300

    Add files via upload

--- module1hard.py.py
+++ module1hard.py.py
@@ -1,7 +1,5 @@
-A=[5,3,3,5,4]
-B=[2,2,2,3]
-J=[4,5,5,2]
-K=[4,4,3]
-S=[5,5,5,4,5,]
-students={"Aaron":sum(A)/len(A),"Bilbo":sum(B)/len(B),"Jonny":sum(J)/len(J),"Kendrik":sum(K)/len(K),"Stive":sum(S)/len(S)}
-print(students)
+g = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+s = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', "Aaron"}
+s=list(s)
+list.sort(s)
+print(s[0],":",sum(g[0])/len(g[0]),s[1],":",sum(g[1])/len(g[1]),s[2],":",sum(g[2])/len(g[2]),s[3],":",sum(g[3])/len(g[3]),s[4],":",sum(g[4])/len(g[4]))
