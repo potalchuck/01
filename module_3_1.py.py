07a5ae63ab2bcad0b19d585c77dbd1bae2c18b87
potalchuck <potalchuck@mail.ru>
2024-07-13 15:54:28 +0300

    Add files via upload

---
+++ module_3_1.py.py
@@ -0,0 +1,18 @@
+calls = 0
+def count_calls():
+        global calls
+        calls += 1
+def string_info(string):
+        count_calls()
+        a = len(string)
+        return a,string.upper(),string.lower()
+def is_contains(string , list_to_search):
+        count_calls()
+        string = string.lower()
+        list_to_search = [item.lower() for item in list_to_search]
+        return  string in list_to_search
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))# Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
