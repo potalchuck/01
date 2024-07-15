7207b65310fdf371dc3cb22c8a4c665b59afd704
potalchuck <potalchuck@mail.ru>
2024-07-15 13:49:50 +0300

    Add files via upload

---
+++ module_3_3.py.py
@@ -0,0 +1,14 @@
+def print_params(a=1, b='строка', c=True):
+  print(a, b, c)
+print_params()
+print_params(10)
+print_params(10, 'другая строка')
+print_params(10, 'другая строка', False)
+print_params()
+print_params()
+values_list = [2, 'новый текст', False]
+values_dict = {'a': 5, 'b': 'еще один текст', 'c': True}
+print_params(*values_list)
+print_params(*values_dict)
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
